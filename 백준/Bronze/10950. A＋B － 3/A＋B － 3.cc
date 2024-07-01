12c30fd44dbff9a06c594bb6219cf00ff2304b6d
zeehyee <103031841+zeehyee@users.noreply.github.com>
2024-07-01 22:03:25 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	
+	int n, i, j;
+	cin >> n;
+	
+	for (int k = 0; k < n; k++) {
+		cin >> i >> j;
+		cout << i + j << endl;
+	}
+
+
+}
+
