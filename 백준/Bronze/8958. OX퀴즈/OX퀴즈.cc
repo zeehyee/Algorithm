7e30cb1e0f5e659838183551869bf4e42e113258
zeehyee <103031841+zeehyee@users.noreply.github.com>
2024-07-12 03:51:09 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -1,23 +1,24 @@
 #include<iostream>
 
+
 using namespace std;
 int main() {
-	string s = "";
-	int tmp = 0, cnt = 0;
-
-	int n;
+	string s;
+	int n, tmp=0, cnt = 0;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> s;
-		for (int j = 0; j < s.size(); j++) {
-			if (s[j] == 'O')
-				tmp += (++cnt);
+		for (int j = 0; s[j] != '\0'; j++) {
+			if (s[j] == 'O') {
+				cnt += ++tmp;
+			}
 			else
-				cnt = 0;
-		}
-		cout << tmp << endl;
-		tmp = 0, cnt = 0;
-	}
+				tmp = 0;
 
-	
+		}
+		cout << cnt << "\n";
+		tmp = 0,cnt = 0;
+	}
+	;
+
 }
