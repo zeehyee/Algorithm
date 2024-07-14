5e19359082279da673834b1bb4f018952c8f3d7e
zeehyee <103031841+zeehyee@users.noreply.github.com>
2024-07-15 01:44:59 +0900

    [Bronze II] Title: 콘테스트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5576. 콘테스트/콘테스트.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int w[10], k[10];
+
+	for (int i = 0; i < 10; i++) {
+		cin >> w[i];
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cin >> k[i];
+	}
+
+
+	sort(w, w + 10);
+	sort(k, k + 10);
+
+	cout << w[7] + w[8] + w[9] << " " << k[7] + k[8] + k[9];
+
+}
