25119426f4ebf5e88f442e957ab530e214814c69
zeehyee <103031841+zeehyee@users.noreply.github.com>
2024-08-05 11:39:47 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<queue>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	queue<int> q;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	while (1) {
+		if (q.size() == 1)
+			break;
+		
+		q.pop();
+		q.push(q.front());
+		q.pop();
+
+	}
+	cout << q.front() << endl;
+
+}
