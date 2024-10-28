8e2e803d86af70b852088a0cc710875c7446deb7
zeehyee <103031841+zeehyee@users.noreply.github.com>
2024-10-28 20:28:45 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(),d.end());
+    
+    for(int i =0; i<d.size(); i++){
+        if(budget-d[i]>=0){
+            budget-=d[i];
+            answer++;
+        }
+        else
+             return answer;
+    }
+   
+}
